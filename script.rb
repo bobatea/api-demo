31bdbc922ab1d74a12810a9abddb84e68eae338c
diwei-jiang <shojdw@gmail.com>
2014-07-19 22:26:50 -0400

    test

--- script.rb
+++ script.rb
@@ -3,5 +3,5 @@ require 'json'
 
 get '/sushi.json' do
   content_type :json
-  return {:sushi => ["Maguro", "Hamachi", "Uni", "Saba", "Ebi", "Sake", "Tai"]}.to_json
+  return {:boba => ["我们", "的", "测试", "API", "哈哈哈"]}.to_json
 end
