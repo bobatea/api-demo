45533cc71be63e185c3e841a53682504b2ef5e98
diwei-jiang <shojdw@gmail.com>
2014-08-10 17:29:05 -0400

    pic upload with S3

--- app/apis/pic_api.rb
+++ app/apis/pic_api.rb
@@ -3,28 +3,40 @@
 #
 class PicPicAPI < Grape::API
 
-  post :picupload do
-    new_pic = current_user.pics.new
-    image_file = params[:pic_file]
-    new_pic.s3_filename = "#{image_file.original_filename.downcase}.#{Time.now.to_i.to_s}"
-    obj = $Bucket.objects.create(new_pic.s3_filename, :file => image_file) rescue nil
+  namespace :protected do
 
-    error!('Inner error, S3', 500) unless obj
+    # Auth
+    before do
+      error!('Unauthorized', 401) unless authenticate!
+    end
 
-    obj.acl = :public_read
-    new_pic.url = obj.public_url.to_s
-    new_pic.description = params[:description]
+    post :picupload do
 
-    if new_pic.save
-      {
-        success: true,
-        info: new_pic
-      }
-    else
-      {
-        success: false,
-        messages: new_pic.errors.messages
-      }
+      error!('Token without user', 500) unless current_user
+
+      new_pic = current_user.pics.new
+      image_file = params[:pic_file]
+
+      new_pic.s3_filename = "#{image_file.filename.downcase}.#{Time.now.to_i.to_s}"
+      obj = $Bucket.objects.create(new_pic.s3_filename, :file => image_file.tempfile) rescue nil
+
+      error!('Inner error, S3', 500) unless obj
+
+      obj.acl = :public_read
+      new_pic.url = obj.public_url.to_s
+      new_pic.description = params[:description]
+
+      if new_pic.save
+        {
+          success: true,
+          info: new_pic
+        }
+      else
+        {
+          success: false,
+          messages: new_pic.errors.messages
+        }
+      end
     end
   end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,3 +1,7 @@
 def session
   env['rack.session']
 end
+
+def current_user
+  @token.user rescue nil
+end
